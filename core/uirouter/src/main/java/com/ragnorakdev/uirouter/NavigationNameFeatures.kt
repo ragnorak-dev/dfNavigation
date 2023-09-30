cb263b9aa260450c8ab68ee2f0ece5addbfc0c7d
ragnorak-dev <raulillangarcia@gmail.com>
2023-09-30 19:34:25 +0200

    :sparkles: created navigation system (#3)

---
+++ core/uirouter/src/main/java/com/ragnorakdev/uirouter/NavigationNameFeatures.kt
@@ -0,0 +1,7 @@
+package com.ragnorakdev.uirouter
+
+enum class NavigationNameFeatures(val moduleName: String) {
+    FEATURE_ONE("dynamicfeature_one"),
+    FEATURE_TWO("dynamicfeature_two"),
+    FEATURE_THREE("dynamicfeature_three")
+}
