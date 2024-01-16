a0ab9cea5f132293805dfb468bd36cde4ad62aa3
Rau00 <raulillangarcia@gmail.com>
2024-01-16 12:18:14 +0100

    :fire: removed unused module

--- core/dfmanager/src/main/java/com/ragnorakdev/dfmanager/LoaderDynamicEntriesPoints.kt
+++
@@ -1,8 +0,0 @@
-package com.ragnorakdev.dfmanager
-
-class LoaderDynamicEntriesPoints {
-    fun execute(loaderPath: String) {
-        val clazz = Class.forName(loaderPath)
-        clazz.getConstructor().newInstance()
-    }
-}
